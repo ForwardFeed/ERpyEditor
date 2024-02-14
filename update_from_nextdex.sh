924579e3798d2c3c2a453d0d9e3830f96a487652
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2024-02-14 17:24:00 +0100

    .gitignore is now working

--- update_from_nextdex.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-BASE="/media/notalinux/sdb3/Programation/TypeScript/nextdex/static/"
-rsync -av --exclude={"js/data","js/index.js","js/data_version.js","js/settings.js","index.html"} ${BASE} ./front/
